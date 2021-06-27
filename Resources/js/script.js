47295a7a18df5c0986c0e58a51c7d7bbb4ad84ea
toanvocuc <toan31311@gmail.com>
2021-06-27 22:32:58 +0700

    cai gi do

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -48,3 +48,20 @@ function mySticky() {
     logo.style.height="75px";
   }
 }
+
+    $(function () {
+        $("#slider-range").slider({
+            range: true,
+            min: 0,
+            max: 1000,
+            values: [190, 728],
+            slide: function (event, ui) {
+                $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+                var mi = ui.values[0];
+                var mx = ui.values[1];
+                filterSystem(mi, mx);
+            }
+        });
+        $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+            " - $" + $("#slider-range").slider("values", 1));
+    });
